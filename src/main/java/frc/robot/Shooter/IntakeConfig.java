b63bea2f090aa81b55a3f2aab66ae4425b639722
Kix4Christ <kix11@icloud.com>
2023-03-17 19:24:03 -0400

    tweak

--- src/main/java/frc/robot/Shooter/IntakeConfig.java
+++ src/main/java/frc/robot/Shooter/IntakeConfig.java
@@ -4,7 +4,7 @@ public final class IntakeConfig
 {
     // shooter
     public static final double level1Speed = 0.7;
-    public static final double level2Speed = 0.8;
+    public static final double level2Speed = 0.85;
     public static final double level3Speed = 1;
 
     public static final double intakeSpeed = 0.5;
